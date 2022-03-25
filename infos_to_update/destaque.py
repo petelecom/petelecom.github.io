c8cd0329633acc5df66ca970bd67f0e536ca7d4b
Guilherme Ferreira <gf154375@gmail.com>
2022-03-25 20:29:51 -0300

    update site

--- infos_to_update/destaque.py
+++ infos_to_update/destaque.py
@@ -8,7 +8,8 @@ id: nome do item
 """
 
 destaque = [
+	{'type': 'local', 'link': './concurso.html', 'id': 'Concurso 50 anos FEELT', 'banner': 'concurso.png'},
 	{'type': 'gdrive', 'link': 'https://youtu.be/W8QS_akt_30', 'id': 'Roda de Conversa Saúde Mental', 'banner': 'https://drive.google.com/file/d/147D3QK1Mua_l327rv35S7P8hrQ01peaX/view?usp=sharing'},
-	{'type': 'gdrive', 'link': 'https://pet-telecom.coursify.me/courses/minicurso-octave', 'id': 'Minicurso Octave', 'banner': 'https://drive.google.com/file/d/1JyBi88rJNpd-m-Ylsx1nEOTn_CrMgdnH/view?usp=sharing'},
+	#{'type': 'gdrive', 'link': 'https://pet-telecom.coursify.me/courses/minicurso-octave', 'id': 'Minicurso Octave', 'banner': 'https://drive.google.com/file/d/1JyBi88rJNpd-m-Ylsx1nEOTn_CrMgdnH/view?usp=sharing'},
 	{'type': 'local', 'link': '#', 'id': 'Resultado Processo Seletivo', 'banner': 'resultadoPS.png'},
 ]
